3e9834b4f443ae9152953d0a4f0d351c32275b4b
Reginald Daniel <redaniel@valdosta.edu>
2013-03-30 23:27:57 -0400

    Error

--- src/com/example/alumniconnectionapp/LoginActivity.java
+++ src/com/example/alumniconnectionapp/LoginActivity.java
@@ -3,15 +3,47 @@ package com.example.alumniconnectionapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ViewFlipper;
 
 public class LoginActivity extends Activity {
-
+	private ViewFlipper flipper; 
+	private EditText email; 
+	private EditText password; 
+	private Button signIn; 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-	}
+		
+		//Set flipper
+		flipper = ((ViewFlipper) this.findViewById(R.id.flipper));
+		flipper.setInAnimation(AnimationUtils.loadAnimation(this,
+				R.anim.push_left_in));
+		flipper.setOutAnimation(AnimationUtils.loadAnimation(this,
+				R.anim.push_left_out));
+		flipper.startFlipping();
+		
+		email = (EditText)findViewById(R.id.email); 
+		password = (EditText)findViewById(R.id.password); 
+		signIn = (Button)findViewById(R.id.sign_in_button); 
+		
+		//set listener for sign in button
+		signIn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+			}
 
+		}); 
+		
+	}//end of onCreate
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
