e8ce0f9eb229163914744866bb141a8201cbcb5f
Reginald Daniel <redaniel@valdosta.edu>
2013-04-11 11:56:51 -0400

    Added account info layout file

--- gen/com/example/alumniconnectionapp/R.java
+++ gen/com/example/alumniconnectionapp/R.java
@@ -29,9 +29,10 @@ public final class R {
         public static final int sign_in_button=0x7f090003;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_users=0x7f030001;
-        public static final int navtabs=0x7f030002;
+        public static final int activity_account_info=0x7f030000;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_users=0x7f030002;
+        public static final int navtabs=0x7f030003;
     }
     public static final class menu {
         public static final int activity_home_page=0x7f080000;
