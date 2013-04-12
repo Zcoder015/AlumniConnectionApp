71d637c0cbf9a6d1b6235c8f9df9a72300a5152d
Reginald Daniel <redaniel@valdosta.edu>
2013-04-12 03:09:53 -0400

    User can now view posts and other users

--- src/com/example/alumniconnectionapp/HomeActivity.java
+++ src/com/example/alumniconnectionapp/HomeActivity.java
@@ -1,16 +1,94 @@
 package com.example.alumniconnectionapp;
 
+import java.util.List;
+
+import data.Post;
+import data.PostFetcher;
+
 import android.app.Activity;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends ListActivity {
+	
+	private TextView empty; 
+	private PostsAdapter adapter;
+	private ProgressDialog pd; 
+	List<Post> posts; 
+	private static final int MENU_ADD_POST = Menu.FIRST;
+	
+ /*	private final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(final Message msg) {
+            pd.dismiss();
+            if ((posts == null) || (posts.size() == 0)) {
+                empty.setText("No Data");
+            } else {
+                adapter = new PostsAdapter(HomeActivity.this, posts);
+                setListAdapter(adapter);
+            }
+        }
+    };   */ 
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState); 
-		TextView textView = new TextView(this);
-		textView.setText("Welcome Home!!"); 
-		setContentView(textView); 
+		setContentView(R.layout.activity_home); 
+		
+		empty = (TextView)findViewById(R.id.empty2); 
+		
+		//set list properties
+		final ListView listview = getListView(); 
+		listview.setItemsCanFocus(false); 
+		listview.setChoiceMode(ListView.CHOICE_MODE_SINGLE); 
+		listview.setEmptyView(this.empty); 
+		
+		
 	}
+	
+	public void onResume(){
+		super.onResume(); 
+		
+		loadPosts(); 
+	}
+	
+	protected void onListItemClick(ListView l, View v, int position, long id){
+		
+	}
+	
+	public boolean onCreateOptionsMenu(Menu menu){
+		super.onCreateOptionsMenu(menu); 
+		
+		menu.add(0, HomeActivity.MENU_ADD_POST, 0, R.string.menu_add_post).setIcon(
+	            android.R.drawable.ic_menu_add); 
+		
+		return true; 
+	}
+	
+	private void loadPosts(){
+		
+	//	pd = ProgressDialog.show(this, "", "Loading...", true, false);
+		final PostFetcher pf = new PostFetcher(); 
+		
+		posts = pf.getPostFromFile("/data/data/com.example.alumniconnectionapp/files/UserPosts.json");
+		adapter = new PostsAdapter(HomeActivity.this, posts); 
+		setListAdapter(adapter); 
+		
+		
+	/*	new Thread() {
+            @Override
+            public void run() {
+            	//Retrieve and parse the data from json file
+        	//	posts = pf.getPostFromFile("/data/data/com.example.alumniconnectionapp/files/UserPosts.json"); 
+                handler.sendEmptyMessage(0);
+            }
+        }.start(); */ 
+	} 
 
 }//end of class 
--- src/com/example/alumniconnectionapp/UsersActivity.java
+++ src/com/example/alumniconnectionapp/UsersActivity.java
@@ -6,7 +6,12 @@ import data.User;
 import data.UsersFetcher;
 
 import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -14,31 +19,69 @@ public class UsersActivity extends ListActivity {
 	
 	private TextView empty; 
 	private UsersAdapter adapter; 
+	private ProgressDialog pd; 
 	List<User> usersList; 
+	
+	/* private final Handler handler = new Handler() {
+	        @Override
+	        public void handleMessage(final Message msg) {
+	            pd.dismiss();
+	            if ((usersList == null) || (usersList.size() == 0)) {
+	                empty.setText("No Data");
+	            } else {
+	                adapter = new UsersAdapter(UsersActivity.this, usersList);
+	                setListAdapter(adapter);
+	            }
+	        }
+	    };   */ 
+	
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState); 
 		
-		this.setContentView(R.layout.activity_users); 
+		setContentView(R.layout.activity_users); 
 		
-		this.empty = (TextView)findViewById(R.id.empty); 
+		empty = (TextView)findViewById(R.id.empty); 
 		
 		//set list properties
 		final ListView listview = getListView(); 
 		listview.setItemsCanFocus(false); 
 		listview.setChoiceMode(ListView.CHOICE_MODE_SINGLE); 
 		listview.setEmptyView(this.empty); 
+		
 	}
 	
 	public void onResume(){
 		super.onResume(); 
 		
-		UsersFetcher uf = new UsersFetcher(); 
+		loadUsers(); 
+	}
+	
+	//Shows individual users information when clicked
+	protected void onListItemClick(ListView l, View v, int position, long id){
 		
-		//Retrieve and parse the data from json file
-		this.usersList = uf.getUserFromFile("/data/data/com.example.alumniconnectionapp/files/users.json"); 
+		//Calls Activity to show each individual users account info
+		Intent intent = new Intent(UsersActivity.this, UserDetail.class); 
+		startActivity(intent); 
+	}
+	
+	private void loadUsers(){
+		
+		//pd = ProgressDialog.show(this, "", "Loading...", true, false);
+		final UsersFetcher uf = new UsersFetcher(); 
+		usersList = uf.getUserFromFile("/data/data/com.example.alumniconnectionapp/files/users.json");
 		adapter = new UsersAdapter(UsersActivity.this, usersList); 
-		setListAdapter(adapter); 
+		setListAdapter(adapter);
+		
+		
+	/*	new Thread() {
+            @Override
+            public void run() {
+            	//Retrieve and parse the data from json file
+        		usersList = uf.getUserFromFile("/data/data/com.example.alumniconnectionapp/files/users.json"); 
+              //  handler.sendEmptyMessage(0);
+            }
+        }.start(); */ 
 	}
 
 }//end of class
