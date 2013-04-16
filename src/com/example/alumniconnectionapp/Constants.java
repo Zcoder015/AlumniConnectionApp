2e3cd1e43564ab6c34a0179cc785dc3194ae5ae0
Reginald Daniel <redaniel@valdosta.edu>
2013-04-16 01:11:35 -0400

    Added network package and associated classes

--- src/com/example/alumniconnectionapp/Constants.java
+++ src/com/example/alumniconnectionapp/Constants.java
@@ -3,5 +3,6 @@ package com.example.alumniconnectionapp;
 public class Constants {
 	
 	public static final String USER_POSTS_JSON_FILE = "/data/data/com.example.alumniconnectionapp/files/UserPosts.json";
+	public static final String LOGTAG = "Alumni"; 
 
 }//end of class 
